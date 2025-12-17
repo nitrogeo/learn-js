f7881508e6dec73d2224ab772a492abad8082ccc
nitro <108837401+nitrogeo@users.noreply.github.com>
2025-12-17 14:35:20 -0600

    fix: correct bot display ID and initialize JavaScript variables

---
+++ rps/index.js
@@ -0,0 +1,6 @@
+// rps
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
